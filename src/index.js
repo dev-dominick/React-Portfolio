35bb041d8ab5b7c648ab2f830cc2dfc8470d2e9f
Dom <Domalbano35@gmail.com>
2022-05-15 16:36:49 -0400

    imputed nav and footer, wrapped app in browser router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// routing
+import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// importing nav and footer components
+import Navbar from './components/navbar.js'
+import Footer from './components/footer.js'
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Navbar />
+      <App />
+      <Footer />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
