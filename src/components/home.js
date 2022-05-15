2b0fd3817febd50b1bdcd2e39fc42fa40e762e49
Dom <Domalbano35@gmail.com>
2022-05-15 18:39:29 -0400

    added text to home page

--- src/components/home.js
+++ src/components/home.js
@@ -9,6 +9,9 @@ export default function Home() {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <AdbIcon className="App-logo" sx={{ display: { xs: "none", md: "flex" }, mr: 5, width: '1000px' }} />
+                <main>
+                  <h1>Welcome! To my New Awesome Portfolio</h1>
+                </main>
             </header>
         </div>
   )
