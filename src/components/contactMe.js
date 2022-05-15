ed7c3f1ffe48bb64b45108845a112a543aa91850
Dom <Domalbano35@gmail.com>
2022-05-15 18:39:47 -0400

    added input fields to contact page

--- src/components/contactMe.js
+++ src/components/contactMe.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import * as React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 
 export default function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <Box
+      component="form"
+      sx={{
+        "& > :not(style)": { m: 1, width: "25ch" },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="outlined-basic" label="First Name" variant="outlined" />
+      <TextField id="outlined-basic" label="Last Name" variant="outlined" />
+      <TextField id="outlined-basic" label="Phone Number" variant="outlined" />
+      <TextField id="outlined-basic" label="Email" variant="outlined" />
+
+    </Box>
+  );
 }
