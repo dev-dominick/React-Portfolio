c2a27168bd0266e44eec9898e1cb8b324d7b6d6c
Dom <Domalbano35@gmail.com>
2022-05-18 10:36:11 -0400

    importing particlejs to my about page

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import './aboutMe.css'
+import ParticlesContainer from "./particles.js";
+
 export default function About() {
   return (
     <div>
-      <h1 className='header'>About Me</h1>
-      <p className='aboutMeText'>Hello World! My name is Dominick Albano and I am a full-stack MERN developer.</p>
+      <ParticlesContainer />
+      <h1 className="header">About Me</h1>
+      <p className="aboutMeText">
+        Hello World! My name is Dominick Albano and I am a full-stack MERN
+        developer.
+      </p>
     </div>
-  )
+  );
 }
