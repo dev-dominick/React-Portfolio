0d6c69f3d19f7c1120c9dd56ae5ae6e18f20f01f
Dom <Domalbano35@gmail.com>
2022-05-17 22:22:49 -0400

    added info to about me page

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -1,7 +1,10 @@
 import React from 'react'
-
+import './aboutMe.css'
 export default function About() {
   return (
-    <div>About</div>
+    <div>
+      <h1 className='header'>About Me</h1>
+      <p className='aboutMeText'>Hello World! My name is Dominick Albano and I am a full-stack MERN developer.</p>
+    </div>
   )
 }
