39858665193e5b2aa3c5280ff7401a0b50831edc
Dom <Domalbano35@gmail.com>
2022-06-22 23:49:06 -0400

    add more info to about me

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -9,8 +9,20 @@ export default function About() {
       <main className="aboutme-header">
         <h1 className="header">About Me</h1>
         <p className="aboutMeText">
-          Hello World! My name is Dominick Albano and I am a full-stack MERN
-          developer.
+          Full-stack web developer leveraging an array of transferable skills
+          from former career. These skills include leadership, a strong teamwork
+          ethic, and the ability to learn as you go. Known as an innovative
+          problem solver, passionate about developing apps, with a focus on
+          mobile-first design and development. Excited to learn new technologies
+          and implement them into future personal and open-source projects.
+          Recently earned a certificate in full stack development from the
+          University of Pennsylvania, with newly developed skills in JavaScript,
+          CSS, React.js, Express, and responsive web design. I have experience
+          with certain aspects of UX design by designing wireframes in order to
+          build the front-end of the app. I also have used Kanban boards,
+          similar to agile development, in many of my projects. Excited to learn
+          new technologies and implement them into future personal and
+          open-source projects.
         </p>
       </main>
       {/* </header> */}
