d518b5082af838e74edcacfc7bb67c85405b6028
Dom <Domalbano35@gmail.com>
2022-07-17 10:40:25 -0400

    removed commented out a tags on project file

--- src/components/projects.js
+++ src/components/projects.js
@@ -8,66 +8,20 @@ import Notepad from './notepad';
 
 export default function Projects() {
   return (
-    //  <div>Projects</div>
+    
     <div>
       <section class="section-area" id="projects">
         <div class="div-container">
           <Dailymealplanner/>
-          {/* <a
-            href="https://dev-dominick.github.io/daily-meal-planner/"
-            class="box-item refactor"
-            id="project-1"
-          >
-            <div className="project-name-text">
-              <h3>Daily Meal Planner </h3>
-              <span>HTML/CSS/JS</span>
-            </div>
-          </a> */}
+        
           <Workdayplanner/>
-          {/* <a
-            href="https://dev-dominick.github.io/work-day-planner/"
-            class="box-item led"
-            id="project-2"
-          >
-            <div className="project-name-text">
-              <h2>Work Day Planner</h2>
-
-              <span>HTML/CSS/JS</span>
-            </div>
-          </a> */}
+        
           <Passwordgenerator/>
-          {/* <a
-            href="https://dev-dominick.github.io/password-generator/"
-            class="box-item calc"
-            id="project-3"
-          >
-            <div className="project-name-text">
-              <h3>Password Generator</h3>
-              <span>HTML/CSS/JS</span>
-            </div>
-          </a> */}
+       
           <Codequiz/>
-          {/* <a
-            href="https://dev-dominick.github.io/code-quiz/"
-            class="box-item puzzles"
-            id="project-4"
-          >
-            <div className="project-name-text">
-              <h3>Code Quiz</h3>
-              <span>HTLM/CSS/JS</span>
-            </div>
-          </a> */}
+        
           <Notepad/>
-          {/* <a
-            href="https://dev-dominick.github.io/horiseon-accessibility-refactor/"
-            class="box-item buddy"
-            id="project-5"
-          >
-            <div className="project-name-text">
-              <h3>Horiseon Refactor</h3>
-              <span>HTML/CSS</span>
-            </div>
-          </a> */}
+       
         </div>
       </section>
     </div>
